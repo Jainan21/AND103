61950a956a2ff14074e9179263342216f9ec4195
Jainan21 <143272157+Jainan21@users.noreply.github.com>
2024-11-30 09:19:29 +0700

    upload

--- routes/products.js
+++ routes/products.js
@@ -32,17 +32,17 @@ router.get("/quantity", async function (req, res) {
 });
 
 router.post('/upload', [upload.single('image')],
-    async(req, res, next) => {
-        try{
-            const{ file } = req;
-            if(!file){
-                return res.json({status: 0, link: ""});
-            }else{
-                const url = `https://and103-1.onrender.com/images/${file.filename}`;
-                return res.json({status: 1, url: url});
+    async (req, res, next) => {
+        try {
+            const { file } = req;
+            if (!file) {
+               return res.json({ status: 0, link : "" }); 
+            } else {
+                const url = `http://192.168.1.13:3000/images/${file.filename}`;
+                return res.json({ status: 1, url : url });
             }
-        }catch(error){
-            console.log('Upload image erroe:', error);
-            return res.json({status: 0, link: ""});
+        } catch (error) {
+            console.log('Upload image error: ', error);
+            return res.json({status: 0, link : "" });
         }
-});
+    });
